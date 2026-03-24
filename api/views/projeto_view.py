56157e2cbca7ad9eb946a1ac3fd63e252a263c48
Tiago <ttr100ttr100@gmail.com>
2026-03-24 19:11:04 +0000

    refactor: add require_GET decorator to project views

--- api/views/projeto_view.py
+++ api/views/projeto_view.py
@@ -1,20 +1,17 @@
 from django.http import JsonResponse
+from django.views.decorators.http import require_GET
 from ..services.projeto_svc import listar_projetos, get_resumo_projeto
 
 
+@require_GET
 def listar_projetos_view(request):
-    if request.method != 'GET':
-        return JsonResponse({'error': 'Metodo nao permitido'}, status=405)
-
     search = request.GET.get('search', '')
     projetos = listar_projetos(search)
     return JsonResponse(projetos, safe=False)
 
 
+@require_GET
 def get_resumo_projeto_view(request, projeto_id):
-    if request.method != 'GET':
-        return JsonResponse({'error': 'Metodo nao permitido'}, status=405)
-
     try:
         resumo = get_resumo_projeto(projeto_id)
         return JsonResponse(resumo)
