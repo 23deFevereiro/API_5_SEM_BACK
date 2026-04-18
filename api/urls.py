bae0117eb12b0bcfce60a9c8413d5776e777d3dd
João Pedro <marcondesjpb@gmail.com>
2026-04-18 11:39:09 -0300

    feat(#8): add route for burnup hours endpoint

--- api/urls.py
+++ api/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('api/projetos/<int:projeto_id>/resumo/', projeto_view.get_resumo_projeto_view, name='resumo_projeto'),
     path('api/projetos/<int:projeto_id>/materiais/', projeto_view.get_materiais_projeto_view, name='materiais_projeto'),
     path('api/projetos/<int:projeto_id>/horas-por-funcionario/', horas_view.get_horas_por_funcionario_view, name='horas_por_funcionario'),
+    path('api/projetos/burnup-horas/', horas_view.get_burnup_horas_projetos_view, name='burnup_horas_projetos'),
     path('api/projetos/<int:projeto_id>/funcionarios/', funcionario_view.get_funcionarios_projeto_view, name='funcionarios_projeto'),
 ]
