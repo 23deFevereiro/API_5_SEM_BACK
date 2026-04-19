604938a482676e37479c9f84fc863fcdef748032
augustopiatto <ampiatto@hotmail.com>
2026-04-19 22:45:25 +0000

    test(#14): fix relative import

--- api/tests/commands/test_fix_csv.py
+++ api/tests/commands/test_fix_csv.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'management', 'commands'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'management', 'commands'))
 
 from fix_csv import (
     criar_pasta_saida,
