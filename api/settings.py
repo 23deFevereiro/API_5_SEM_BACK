e0ce6d88a58cbe8584a7236065071b9c7e92bda6
Matheus Marciano <mm.leitea@gmail.com>
2026-05-11 06:04:35 +0000

    chore: remove config from settings script

--- api/settings.py
+++ api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-63)8h20b6bh5%qori9b$&n)c_o2zden(m0(#f5n5rs(trg-3pj'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
