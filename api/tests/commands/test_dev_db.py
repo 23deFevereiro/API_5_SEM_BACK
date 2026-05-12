ecfd5769d0304081a27bda6160405089f00524ed
Augusto de Moraes Piatto <ampiatto@hotmail.com>
2026-05-11 21:32:07 -0300

    test(#27): fix test dev db

--- api/tests/commands/test_dev_db.py
+++ api/tests/commands/test_dev_db.py
@@ -43,7 +43,6 @@ class TestGetConnection:
         'POSTGRES_PORT': '5432',
         'POSTGRES_DB': 'test_db',
         'POSTGRES_USER': 'test_user',
-        'POSTGRES_PASSWORD': 'dummy-value-for-testing',  # nosonar
     })
     @patch('api.management.commands.seeds.seed_0005.psycopg2.connect')
     def test_chama_psycopg2_connect(self, mock_connect):
