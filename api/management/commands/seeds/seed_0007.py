69c74305dd7cc492dd05c637af8c2ce23da212f5
Augusto de Moraes Piatto <ampiatto@hotmail.com>
2026-05-29 01:48:50 +0000

    chore(#42): change model

---
+++ api/management/commands/seeds/seed_0007.py
@@ -0,0 +1,4 @@
+def run():
+    print(
+        f"\n📦 Seed 07 - Carregando dimensões e fatos do Star Model (com data_inicio/data_fim_prevista em dim_projeto)..."
+    )
--- api/models/dim_fornecedor.py
+++ api/models/dim_fornecedor.py
@@ -4,7 +4,7 @@ from django.db import models
 class DimFornecedor(models.Model):
     id = models.IntegerField(primary_key=True)
     codigo_fornecedor = models.CharField(max_length=20, unique=True)
-    razao_social = models.CharField(max_length=100)
+    razao_social = models.CharField(max_length=120)
     cidade = models.CharField(max_length=50, blank=True)
     estado = models.CharField(max_length=2, blank=True)
     categoria = models.CharField(max_length=50, blank=True)
